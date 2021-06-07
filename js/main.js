53d821a21d76c37538d73735cc57998d96f73c35
shafe <alam.shafee@gmail.com>
2021-06-07 18:27:27 +0600

    implemented details button function on project section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 "use strict";
+document.querySelectorAll(".btn-project-details").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("change");
+    btn.nextElementSibling.classList.toggle("change");
+  });
+});
